bec40ae064a680da02c444ef597c90a19ce3bbba
Angel Paredes <aparedes89@outlook.com>
2017-02-03 22:34:57 -0500

    separate identity model classes

---
+++ Code/MyVipCity/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MyVipCity.Models {
+
+	public class ApplicationDbContext: IdentityDbContext<ApplicationUser> {
+
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false) {
+		}
+
+		public static ApplicationDbContext Create() {
+			return new ApplicationDbContext();
+		}
+	}
+}
