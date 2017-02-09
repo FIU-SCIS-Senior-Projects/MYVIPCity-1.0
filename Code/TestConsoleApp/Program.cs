8475863b24a05b54df61fe389e53dc678678a640
Angel Paredes <aparedes89@outlook.com>
2017-02-08 20:46:34 -0500

    Create Console app for testing

---
+++ Code/TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyVipCity.Mailing.Sendgrid;
+
+namespace TestConsoleApp {
+	class Program {
+		private static void Main(string[] args) {
+			var apiKey = ConfigurationManager.AppSettings["myvipcity:send-grid-api"];
+			SendGridEmailService sendGridEmailService = new SendGridEmailService(apiKey);
+			Console.ReadLine();
+		}
+	}
+}
