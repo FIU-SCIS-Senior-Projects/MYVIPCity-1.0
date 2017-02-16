0a0293e849e35e0cb5d61a5572c78e073c610610
Angel Paredes <aparedes89@outlook.com>
2017-02-15 21:45:31 -0500

    Add [Authorize(Roles = "Admin")] to AddBusinessController

--- Code/MyVipCity/Controllers/AddBusinessController.cs
+++ Code/MyVipCity/Controllers/AddBusinessController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MyVipCity.Controllers
 {
+	[Authorize(Roles = "Admin")]
     public class AddBusinessController : Controller
     {
         // GET: AddBusiness
