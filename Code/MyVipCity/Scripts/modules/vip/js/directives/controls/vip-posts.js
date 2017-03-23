73beb6b4375ac56dadc074d6ffece0115db88cdb
Angel Paredes <aparedes89@outlook.com>
2017-03-22 23:45:24 -0400

    more work on posts

--- Code/MyVipCity/Scripts/modules/vip/js/directives/controls/vip-posts.js
+++ Code/MyVipCity/Scripts/modules/vip/js/directives/controls/vip-posts.js
@@ -1,5 +1,11 @@
-﻿define(['vip/js/vip', 'jquery', 'angular'], function (vip, jQuery, angular) {
+﻿define(['vip/js/vip'], function (vip) {
 	'use strict';
+
+	vip.postsTypes = {
+		comment: 'C',
+		video: 'V',
+		picture: 'P'
+	};
 
 	vip.directive('vipPosts', ['$http', function ($http) {
 		return {
@@ -16,40 +22,109 @@
 				'</div>' +
 				'<div class="card">' +
 					'<div class="card__body">' +
-						'<form name="postCommentForm" ng-show="_showPostComment">' +
+						'<form name="postCommentForm" ng-show="_showPost == \'C\'">' +
 							'<div required vip-textarea ng-model="_commentPost.Comment" edit-mode-class="form-control textarea-autoheight" vip-edit-only maxlength="1000" placeholder="Write your comment here..."></div>' +
 							'<button class="btn vip-posts__save-post-btn" ng-disabled="postCommentForm.$invalid" ng-click="saveCommentPost($event)">Post</button>' +
 						'</form>' +
 					'</div>' +
+				'</div>' +
+				// '<div infinite-scroll="loadMorePosts()" infinite-scroll-distance="0" infinite-scroll-immediate-check="false">' +
+				'<div class="vip-posts__posts">' +
+					'<div class="card {{::post.PostType}}" ng-repeat="post in posts track by post.Id">' +
+						'<div class="card__body">' +
+							'<div vip-post></div>' +
+						'</div>' +
+					'</div>' +
+					'<div class="load-more">' +
+                        '<a href="" ng-click="loadMorePosts()"><i class="zmdi zmdi-refresh-alt"></i> Load more</a>' +
+                    '</div>' +
 				'</div>'
 				,
 
 			link: function (scope) {
 				var listeners = [];
 
-				var afterPostAdded = function() {
+				var topLoad = 3;
+				var businessId;
+				scope.posts = [];
+				scope.loadingPosts = false;
+
+				// load more posts
+				scope.loadMorePosts = function () {
+					// check if there is not a loading operation currently in progress
+					if (!scope.loadingPosts) {
+						// indicate a new loading operation
+						scope.loadingPosts = true;
+						// get the url to retrieve the posts
+						var url = ('api/Business/Posts/' + businessId + '/' + topLoad) + (scope.posts === null || !scope.posts.length ? '' : '/' + scope.posts[scope.posts.length - 1].Id);
+						// make request to retrieve the posts
+						$http.get(url).then(function (response) {
+							// update posts array
+							if (response.data) {
+								scope.posts = scope.posts.concat(response.data);
+								// TODO: Should we scroll to after the element before adding the new posts, i.e current 5, added 3, scroll to item 6
+							}
+						})['finally'](function () {
+							// indicate that loading operation has finished
+							scope.loadingPosts = false;
+						});
+					}
+				};
+
+				// refresh the posts
+				var refreshPosts = function () {
+					// set empty posts
+					scope.posts = [];
+					// load more posts
+					scope.loadMorePosts();
+				}
+
+				var afterPostAdded = function () {
 					scope._commentPost = { Comment: null };
-					scope._showPostComment = false;
+					scope._showPost = null;
 
 					// TODO: Refresh posts
 				};
 
 				scope.clickPostComment = function () {
 					scope._commentPost = { Comment: null };
-					scope._showPostComment = true;
+					scope._showPost = vip.postsTypes.comment;
 				};
 
-				scope.saveCommentPost = function() {
-					$http.post('api/Business/' + scope.model.Id + '/PostComment', scope._commentPost).then(function() {
+				scope.saveCommentPost = function () {
+					$http.post('api/Business/' + businessId + '/PostComment', scope._commentPost).then(function () {
 						afterPostAdded();
 					});
 				};
+
+				var unregister = scope.$watch('model.Id', function (id) {
+					if (id) {
+						businessId = id;
+						refreshPosts();
+						unregister();
+					}
+				});
 
 				listeners.push(scope.$on('$destroy', function () {
 					// unregister listeners
 					for (var i = 0; i < listeners.length; i++)
 						listeners[i]();
 				}));
+			}
+		};
+	}]);
+
+	vip.directive('vipPost', [function () {
+		return {
+			restrict: 'ACE',
+
+			// new scope
+			scope: true,
+
+			template: '{{post.Comment}}',
+
+			link: function(scope, element, attrs) {
+				
 			}
 		};
 	}]);
--- Code/MyVipCity/Scripts/modules/vip/js/vip.js
+++ Code/MyVipCity/Scripts/modules/vip/js/vip.js
@@ -1,7 +1,7 @@
-﻿define(['angular', 'angular-resource', 'angular-messages', 'angular-animate', 'angular-route', 'angular-sanitize', 'angular-material', 'ng-on'], function (angular) {
+﻿define(['angular', 'angular-resource', 'angular-messages', 'angular-animate', 'angular-route', 'angular-sanitize', 'angular-material', 'ng-on', 'ng-infinite-scroll'], function (angular) {
 	'use strict';
 
-	var vipModule = angular.module('vip', ['ng', 'ngResource', 'ngMessages', 'ngAnimate', 'ngRoute', 'ngSanitize', 'ngMaterial', 'argshook.ngOn']);
+	var vipModule = angular.module('vip', ['ng', 'ngResource', 'ngMessages', 'ngAnimate', 'ngRoute', 'ngSanitize', 'ngMaterial', 'argshook.ngOn', 'infinite-scroll']);
 
 	vipModule.renderingModes = {
 		read: 1,
